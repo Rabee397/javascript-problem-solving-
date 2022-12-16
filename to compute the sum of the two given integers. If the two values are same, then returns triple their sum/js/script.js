2c7c8288a97e2be7c45dc4f97d619a02b970d9a3
RABEE KHALIFA <110689058+Rabee397@users.noreply.github.com>
2022-12-16 17:43:45 +0200

    Add files via upload

---
+++ to compute the sum of the two given integers. If the two values are same, then returns triple their sum/js/script.js
@@ -0,0 +1,11 @@
+function code(x,y){
+    if(x ==y){
+        return 3 *(x+y)
+    }else{
+        return ("they are not same")
+    }
+}
+
+document.write(code(10,10))
+document.write("<br> ")
+document.write(code(1,2))
